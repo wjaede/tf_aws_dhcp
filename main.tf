1445dcac3bf776f060c21b9f93331cb68a813e66
wolfgang.jaede <wolfgang.jaede@bauerxcel.de>
2015-09-26 19:12:33 +0200

    multi regeion update

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+/* Setup our aws provider */
+provider "aws" {
+  access_key  = "${var.access_key}"
+  secret_key  = "${var.secret_key}"
+  region      = "${var.region}"
+}
 resource "aws_vpc_dhcp_options" "internal" {
   domain_name = "${var.internal_domain}"
   domain_name_servers = ["AmazonProvidedDNS"]
--- variables.tf
+++ variables.tf
@@ -1,2 +1,6 @@
 variable "vpc_id" {}
 variable "internal_domain" {}
+/* provider */
+variable "region"               {}
+variable "access_key"           {}
+variable "secret_key"           {}
